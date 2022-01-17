2cc7106bc84bf401a87ca6d5c68d063d17e20c2e
PrasathB-27 <90265595+PrasathB-27@users.noreply.github.com>
2022-01-17 13:24:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,161 @@
+let loc =document.getElementById("location");
+let tempicon=document.getElementById("temp-icon");
+let tempvalue=document.getElementById("temp-value");
+let climate =document.getElementById("climate");
+
+let windspeed =document.getElementById("wind-speed");
+let humid =document.getElementById("humidity");
+
+let iconfile;
+const searchInput=document.getElementById("search-input");
+const searchButton=document.getElementById("search-button");
+searchButton.addEventListener('click', (e)=>
+{
+e.preventDefault();
+getWeather(searchInput.value);
+searchInput.value='';
+});
+
+
+const getWeather=async (city)=>
+{
+    try{
+
+        const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ae925465aa8ae1521f33f3ec056f4f65`,
+            {mode: 'cors'}
+        );
+
+        const weatherData= await response.json();
+        console.log(weatherData);
+        const{name}=weatherData;
+        const{feels_like}=weatherData.main; 
+        const{humidity}=weatherData.main; 
+        const{id,main}=weatherData.weather[0];
+        const{speed}=weatherData.wind;  //
+        loc.textContent=name;
+        climate.textContent=main;
+        tempvalue.textContent=Math.round(feels_like-273);
+        
+        
+
+        if(id<300 && id>200)
+        {
+            tempicon.src="./icons/thunderstorm.png"
+        }
+       else  if(id<400 && id>300)
+        {
+            tempicon.src="./icons/atmospheric-pollution.png"
+        }
+       else if(id<600 && id>500)
+        {
+            tempicon.src="./icons/rain.png"
+        }
+       else  if(id<700 && id>600)
+        {
+            tempicon.src="./icons/snowfall.png"
+        }
+       else  if(id<800 && id>700)
+        {
+            tempicon.src="./icons/cloud.png"
+        }
+         else if(id==800.3)
+        {
+            tempicon.src="./icons/sun.png"
+        }
+
+        windspeed.textContent=speed;
+        humid.textContent=humidity;
+
+
+   
+    }
+catch(error)
+{
+    alert('city not found');
+}
+
+
+
+
+
+};
+
+
+
+window.addEventListener("load" ,()=>{
+
+let long;
+let lat;
+
+if(navigator.geolocation)
+{
+
+    navigator.geolocation.getCurrentPosition((position)=>
+    {
+
+   
+    
+    long=position.coords.longitude;
+    lat=position.coords.latitude;
+    const proxy="https://cors-anywhere.herokuapp.com/";
+
+        const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ae925465aa8ae1521f33f3ec056f4f65     `
+        fetch(api).then((response)=>{
+            return response.json();
+        })
+        .then (data =>
+            {
+                    const{name}=data;
+                    const{feels_like}=data.main;  
+                    const{humidity}=data.main; 
+                    const{id,main}=data.weather[0];
+                    const{speed}=data.wind;  //
+                    loc.textContent=name;
+                    climate.textContent=main;
+                    tempvalue.textContent=Math.round(feels_like-273);
+                  
+                    // const weatherData= await response.json();
+                    // console.log(weatherData);
+                    // const{name}=weatherData;
+                    // const{feels_like,humidity}=weatherData.main;
+                    // const{id,main}=weatherData.weather[0];
+                    // const{wind}=weatherData.wind;
+                    // loc.textContent=name;
+                    // climate.textContent=main;
+                    // tempvalue.textContent=Math.round(feels_like-273);
+                    // tempvalue.textContent=humidity;
+                    // wind.textContent=wind;
+
+
+                    if(id<300 && id>200)
+                    {
+                        tempicon.src="./icons/thunderstorm.png"
+                    }
+                    else  if(id<400 && id>300)
+                    {
+                        tempicon.src="./icons/atmospheric-pollution.png"
+                    }
+                    else if(id<600 && id>500)
+                    {
+                        tempicon.src="./icons/rain.png"
+                    }
+                    else  if(id<700 && id>600)
+                    {
+                        tempicon.src="./icons/snowfall.png"
+                    }
+                    else  if(id<800 && id>700)
+                    {
+                        tempicon.src="./icons/cloud.png"
+                    }
+                    else if(id==800)
+                    {
+                        tempicon.src="./icons/sun.png"
+                    }
+
+                    windspeed.textContent=speed;
+                    humid.textContent=humidity;
+                    console.log(data);
+            })
+}
+    )}
+})
